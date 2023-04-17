496d49ee14ac4447ed3dbd8f75714ebe9209dfc9
Ayush12308 <117008435+Ayush12308@users.noreply.github.com>
2023-04-17 15:29:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("My first console project");
+
+var name = "Ayush";
+console.log(name);
+
+var last_name = "Kumar";
+
+var  full_name=name.concat(last_name);
+console.log(full_name);
+
+var length_of_full_name = full_name.length;
+console.log(length_of_full_name);
+
+var uppercase=full_name.toUpperCase();
+console.log(uppercase);
+
+var a= 8;
+var b= 56;
+var c=a+b;
+console.log(c)
+
+var min_no=Math.min(154, 584);
+console.log(min_no);
+
+var max_no=Math.max(784, 455, 4576);
+console.log(max_no);
+
+var square_root=Math.sqrt(5000);
+console.log(square_root);
